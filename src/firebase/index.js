875044a2b161a8d3e100095270756c5c03038923
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2025-01-06 16:14:18 +0200

    Enable caching.

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore, persistentLocalCache } from "firebase/firestore";
 import firebaseConfig from '@/firebase/config';
 
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+    localCache: persistentLocalCache(),
+});
