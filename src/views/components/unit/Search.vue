fb2f9198de09e30c9a0154afe96f977b0acf5ac7
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2025-01-07 07:04:16 +0200

    Implement component.

---
+++ src/views/components/unit/Search.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-text-field
+      v-model="value"
+      prepend-inner-icon="mdi-magnify"
+      label="Search..."
+  />
+</template>
+
+<script setup>
+const value = defineModel();
+</script>
