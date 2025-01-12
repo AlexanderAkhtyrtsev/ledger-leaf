f22e37df55c99753c3caf777397b00e580d794e6
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2025-01-12 11:46:42 +0200

    Implement transaction model.

---
+++ src/firebase/models/Transaction.js
@@ -0,0 +1,28 @@
+import FirestoreModel from '@/firebase/models/FirestoreModel';
+import {Timestamp} from 'firebase/firestore';
+import {auth} from '@/firebase/auth';
+
+export default class Transaction extends FirestoreModel {
+    constructor({ id, amount, categoryId, accountId, date, note }) {
+        super({
+            id,
+            amount: +amount,
+            categoryId,
+            accountId,
+            date,
+            note
+        });
+    }
+
+    get collectionName() {
+        return`users/${auth.currentUser.uid}/transactions`;
+    }
+
+    normalizedData() {
+        return {
+            ...this,
+            amount: +this.amount,
+            date: this.date instanceof Timestamp ? this.date : Timestamp.fromDate( new Date( this.date ) )
+        }
+    }
+}
