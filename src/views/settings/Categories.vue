cfcc20d4480999ee090b251cdfc428800a640f32
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2025-01-31 07:56:57 +0200

    Implement drag and drop.

--- src/views/settings/Categories.vue
+++ src/views/settings/Categories.vue
@@ -2,8 +2,13 @@
   <v-card>
     <v-card-title>Categories</v-card-title>
     <v-card-text>
-      <v-list>
-        <Category v-for="cat in categories" :category="cat" />
+      <v-list @dragover.prevent>
+        <Category
+            v-for="cat in categories"
+            :category="cat"
+            @drag-start="handleDragStart"
+            @drag-drop="handleDrop"
+        />
       </v-list>
     </v-card-text>
     <CreateCategory />
@@ -11,13 +16,49 @@
 </template>
 
 <script setup>
-import {computed} from 'vue';
+import {computed, ref} from 'vue';
 import store from '@/store';
 import CreateCategory from '@/views/components/modal/CreateCategory.vue';
 import Category from '@/views/settings/unit/Category.vue';
 
 const categories = computed(() => store.getters['database/categories'] );
 
+
+const srcElement = ref();
+
+const handleDragStart = (event) => {
+  event.dataTransfer.setData(
+      'text/plain', JSON.stringify({
+        type: 'category',
+        data: store.getters['database/getCategoryById'](event.target.dataset.categoryId),
+      }),
+  )
+
+  srcElement.value = event.target;
+}
+
+const handleDrop = (event) => {
+  const target = event.target.closest('[data-category-id]');
+alert()
+  if ( !srcElement.value ) return;
+
+  const parentId = target?.dataset?.categoryId || null;
+  const id = srcElement.value.dataset.categoryId;
+
+  srcElement.value = null;
+
+  if ( !id || id === parentId) return;
+
+  const srcCategory = store.getters['database/getCategoryById'](id);
+  const parentCategory = store.getters['database/getCategoryById'](parentId);
+
+  if ( parentCategory?.parentId === srcCategory.id ) return;
+
+  store.dispatch('database/updateCategory', {
+    id,
+    parentId
+  } )
+}
 </script>
 
 <style scoped>
--- src/views/settings/unit/Category.vue
+++ src/views/settings/unit/Category.vue
@@ -1,15 +1,28 @@
 <template>
-  <v-list-item>
+  <v-list-item
+      :data-category-id="cat.id"
+      @dragstart="emit('drag-start', $event)"
+      @drop="emit('drag-drop', $event)"
+      draggable="true"
+  >
     <template v-slot:prepend><v-icon :class="{['ms-' + (2 * (level || 0))]: true}">{{cat.icon}}</v-icon></template>
     <template v-slot:title>{{ cat.name }}</template>
   </v-list-item>
   <v-list v-if="cat.children.length">
-    <Category v-for="child in cat.children" :category="child" :level="(level || 1) + 1" />
+    <Category
+        @drag-start="emit('drag-start', $event)"
+        @drag-drop="emit('drag-drop', $event)"
+        v-for="child in cat.children"
+        :category="child" :level="(level || 1) + 1"
+    />
   </v-list>
 </template>
 
 <script setup>
+const emit = defineEmits(['drag-start', 'drag-drop']);
+
 const {category: cat, level} = defineProps(['category', 'level'])
+
 </script>
 
 <style scoped>
