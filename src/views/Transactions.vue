41e16d30ad24008d58f719edb3c90374c470b8ac
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2025-02-07 14:30:53 +0200

    Rectify component name.

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,11 +1,11 @@
 <template>
   <SwitchPeriod />
-  <TransactionsList />
+  <GroupedTransactionsList />
   <CreateTransaction />
 </template>
 
 <script setup>
 import CreateTransaction from '@/views/components/modal/CreateTransaction.vue';
-import TransactionsList from '@/views/components/TransactionsList.vue';
 import SwitchPeriod from '@/views/components/unit/SwitchPeriod.vue';
+import GroupedTransactionsList from '@/views/components/GroupedTransactionsList.vue';
 </script>
