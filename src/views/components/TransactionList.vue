7ff09c5827be9ea5c2ffd7edae89f8a6948bf23b
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2025-02-07 14:31:17 +0200

    Move to dedicated component.

---
+++ src/views/components/TransactionList.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-list lines="two" v-if="transactionList.length" density="compact">
+    <v-list-item
+        v-for="transaction in transactionList"
+        :key="transaction.id"
+    >
+      <template v-slot:title>{{ displayDescription(transaction, true) }}</template>
+      <template v-slot:subtitle>{{ displayDescription(transaction)}}</template>
+
+      <v-list-item-subtitle class="text-high-emphasis">
+        <Date v-model="transaction.date" />
+      </v-list-item-subtitle>
+
+      <template v-slot:prepend>
+        <v-avatar>
+          <v-icon>{{ transaction.category?.icon }}</v-icon>
+        </v-avatar>
+      </template>
+
+      <template v-slot:append>
+        <div :style="{ color: transaction.amount > 0 ? 'green' : 'red' }">
+          {{ transaction.amount > 0 ? '+' : '' }}
+          <Currency :amount="transaction.amount" :currency="transaction.currency" />
+        </div>
+      </template>
+    </v-list-item>
+  </v-list>
+</template>
+<script setup>
+import Date from "@/views/components/unit/Date.vue";
+import Currency from "@/views/components/unit/Currency.vue";
+
+const transactionList = defineModel();
+
+const displayDescription = (transaction, isTitle) => {
+  const category = (transaction.category ? transaction.category.name : `Transfer ${transaction.amount < 0 ? '<-' : '->'} ` + transaction.account.name);
+  if ( isTitle ) {
+    return transaction.note || category
+  }
+
+  return transaction.note ? category : ''
+}
+</script>
--- src/views/reports/TopExpenses.vue
+++ src/views/reports/TopExpenses.vue
@@ -3,44 +3,15 @@
     <v-card-title>Top Expenses</v-card-title>
 
     <v-card-text>
-      <v-list lines="two" v-if="topExpenses" density="compact">
-        <v-list-item
-            v-for="transaction in topExpenses"
-            :key="transaction.id"
-        >
-          <template v-slot:title>{{ displayDescription(transaction, true) }}</template>
-          <template v-slot:subtitle>{{ displayDescription(transaction)}}</template>
-
-          <v-list-item-subtitle class="text-high-emphasis">
-            <Date v-model="transaction.date" />
-          </v-list-item-subtitle>
-
-          <template v-slot:prepend>
-            <v-avatar>
-              <v-icon>{{ transaction.category?.icon }}</v-icon>
-            </v-avatar>
-          </template>
-
-          <template v-slot:append>
-            <div :style="{ color: transaction.amount > 0 ? 'green' : 'red' }">
-              {{ transaction.amount > 0 ? '+' : '' }}
-              <Currency :amount="transaction.amount" :currency="transaction.currency" />
-            </div>
-          </template>
-
-        </v-list-item>
-      </v-list>
+      <TransactionList :model-value="topExpenses" />
     </v-card-text>
   </v-card>
 </template>
 
 <script setup>
 import store from '@/store';
-import eventBus from '@/eventBus';
-import Currency from '@/views/components/unit/Currency.vue';
 import {computed} from 'vue';
-import {DateTime} from 'luxon';
-import Date from '@/views/components/unit/Date.vue';
+import TransactionList from '@/views/components/TransactionList.vue';
 
 const topExpenses = computed( () => store.getters['database/transactions']
     .filter( t => t.amount < 0 ) // exclude incomes
@@ -48,16 +19,6 @@ const topExpenses = computed( () => store.getters['database/transactions']
     .sort( (t1, t2) => t1.amount < t2.amount ? -1 : 1 )
     .slice(0, 5)
 );
-
-
-const displayDescription = (transaction, isTitle) => {
-  const category = (transaction.category ? transaction.category.name : `Transfer ${transaction.amount < 0 ? '<-' : '->'} ` + transaction.account.name);
-  if ( isTitle ) {
-    return transaction.note || category
-  }
-
-  return transaction.note ? category : ''
-}
 </script>
 
 <style scoped>
