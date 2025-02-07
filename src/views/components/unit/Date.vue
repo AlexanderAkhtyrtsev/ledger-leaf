585aa69c1ddeb01e04b7b3cb7e0efb1654fcc9af
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2025-02-07 14:17:43 +0200

    Implement component.

---
+++ src/views/components/unit/Date.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {Timestamp} from 'firebase/firestore';
+import {computed} from 'vue';
+import {DateTime} from 'luxon';
+
+const model = defineModel();
+
+const formattedDate = computed(() => {
+  return DateTime.fromJSDate(
+      model.value instanceof Timestamp
+      ? model.value.toDate()
+      : model.value
+  ).toFormat('MMMM dd, yyyy').replace( ', ' + (new Date()).getFullYear(), '' )
+})
+</script>
+
+<template>
+  {{ formattedDate }}
+</template>
+
+<style scoped>
+
+</style>
